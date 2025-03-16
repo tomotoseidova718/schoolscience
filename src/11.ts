ec61505095cb59aff646bcbe0896401884cf46cc
tomotoseidova718 <tomotoseidova718@gmail.com>
2025-03-16 15:00:29 +0000

    Add 11.ts

---
+++ src/11.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app-schoolscience',
+    templateUrl: './schoolscience.component.html',
+    styleUrls: ['./schoolscience.component.css']
+})
+export class SchoolscienceComponent{
+}
